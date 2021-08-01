8ae3f5c502928549b31b83907fec2440b925c7dc
zinon-software <business.zinon@gmail.com>
2021-08-01 09:51:27 -0700

    update v9.0.1

--- android/app/build/generated/source/buildConfig/release/com/example/whatsapp_group_links/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/release/com/example/whatsapp_group_links/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.zainon.whatsapp_group_links";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 7;
-  public static final String VERSION_NAME = "4.0.2";
+  public static final int VERSION_CODE = 8;
+  public static final String VERSION_NAME = "8.0.3";
 }
